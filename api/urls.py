21baba0e5a8bf0e7cee9c2211954e47557e5bf1d
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-02-23 12:14:44 +0100

    changed docs url

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,5 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Genstem API')
 
 urlpatterns = [
-    url(r'^$', schema_view),
+    url(r'^docs/', schema_view),
 ]

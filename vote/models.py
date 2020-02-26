bad2eebe6aa047ddc09609520e058ef1d51d39c3
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-02-27 00:56:27 +0100

    added event and election serializers + member election answer vote models

--- vote/models.py
+++ vote/models.py
@@ -10,3 +10,25 @@ class Event(models.Model):
     name = models.CharField(default='', max_length=200)
     startDate = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
+
+class Member(models.Model): # used to let members answer to elections in events
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    authenticated = models.DateTimeField(auto_now=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+class Election(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    description = models.CharField(default='', max_length=3000)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+
+class Answer(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    name = models.CharField(default='', max_length=200)
+
+class Vote(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    election = models.ForeignKey(Event, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
--- vote/serializers.py
+++ vote/serializers.py
@@ -3,10 +3,17 @@ from rest_framework import routers, serializers, viewsets
 from rest_framework import exceptions
 from rest_framework.renderers import JSONRenderer
 
-from vote.models import Event
+from vote.models import Event, Election
+
+class ElectionSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Election
+        fields = ('id', 'active', 'description')
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    election_set = ElectionSerializer(many=True)
     class Meta:
         model = Event
-        fields = ('name',)
+        fields = ('name', 'active', 'startDate', 'election_set',)
 

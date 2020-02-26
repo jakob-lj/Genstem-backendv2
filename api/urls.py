5eebf4fcc1a9c1ba4ee97df3f533a7b62804a505
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-02-26 01:09:14 +0100

    added dev tools resopnse

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,11 @@ from django.urls import path
 from django.conf.urls import url
 from genAuth.views import SSOLogin, CreateUser, VerifyUser, LoginStepTwo
 from rest_framework_swagger.views import get_swagger_view
+from api.views import EnvironmentView
 
 schema_view = get_swagger_view(title='Genstem API')
 
 urlpatterns = [
     url(r'^docs/', schema_view),
+    path('environment/', EnvironmentView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import os
 
 # Create your views here.
+class EnvironmentView(APIView):
+    def get(self, request):
+        """
+        Get environemnt variables
+        """
+        domain = os.environ.get('DOMAIN')
+        if domain:
+            return Response({'domain':domain})
+        else:
+            return Response({'domain':'unknown domain'})
